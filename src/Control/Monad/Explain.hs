651200572ab9f2f765c28eb4b6d72693395664ae
James Henri Haydon <james.haydon@gmail.com>
2021-07-04 14:40:34 +0900

    try some duality

--- src/Control/Monad/Explain.hs
+++ src/Control/Monad/Explain.hs
@@ -3,6 +3,7 @@
 module Control.Monad.Explain where
 
 import Control.Applicative
+import Control.Lens
 import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.State
@@ -10,19 +11,23 @@ import Data.Bifunctor
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Text (Text)
-import qualified Data.Text as Text
+import Prettyprinter
 
 class Explanation e where
   plainly :: e
-  paralleled :: [e] -> e
-  sequenced :: [e] -> e
-  contrasted :: e -> e -> e
+  paralleled :: e -> e -> e
+  sequenced :: e -> e -> e
+
+class Dualistic e where
+  negated :: e -> e
 
 instance Explanation Text where
   plainly = ""
-  paralleled = Text.intercalate ", "
-  sequenced = Text.intercalate "; "
-  contrasted x y = x <> ", but: " <> y
+  paralleled x y = x <> ", " <> y
+  sequenced x y = x <> "; " <> y
+
+instance Dualistic Text where
+  negated = ("not: " <>)
 
 -- | A monad transformer that adds explanations to other monads. A computation
 -- may either fail with an explanation of what went wrong, or succeed with an
@@ -68,8 +73,10 @@ instance (Explanation e, Applicative f) => Applicative (ExplainT e f) where
     where
       ay = go <$> af <*> ax
       go f_ x_ = case (f_, x_) of
-        ((e, Just f), (e', Just x)) -> (paralleled [e, e'], Just (f x))
-        ((e, _), (e', _)) -> (paralleled [e, e'], Nothing)
+        ((e, Just f), (e', Just x)) -> (paralleled e e', Just (f x))
+        ((_, Just _), (e', Nothing)) -> (e', Nothing)
+        ((e, Nothing), (_, Just _)) -> (e, Nothing)
+        ((e, _), (e', _)) -> (paralleled e e', Nothing)
 
 instance (Explanation e, Applicative f) => Alternative (ExplainT e f) where
   empty = failureT plainly
@@ -77,8 +84,8 @@ instance (Explanation e, Applicative f) => Alternative (ExplainT e f) where
     where
       az = go <$> ax <*> ay
       go x_ y_ = case (x_, y_) of
-        ((e, Nothing), (e', Nothing)) -> (paralleled [e, e'], Nothing)
-        ((e, Just x), (e', Just _)) -> (paralleled [e, e'], Just x)
+        ((e, Nothing), (e', Nothing)) -> (paralleled e e', Nothing)
+        ((e, Just x), (e', Just _)) -> (paralleled e e', Just x)
         ((e, Just x), _) -> (e, Just x)
         (_, (e, Just y)) -> (e, Just y)
 
@@ -91,7 +98,7 @@ instance (Explanation e, Monad m) => Monad (ExplainT e m) where
           Nothing -> pure (e, Nothing)
           Just x -> do
             (e', y_) <- runExplainT (f x)
-            pure (sequenced [e, e'], y_)
+            pure (sequenced e e', y_)
 
 instance (Explanation e) => MonadTrans (ExplainT e) where
   lift = ExplainT . fmap ((plainly,) . Just)
@@ -117,7 +124,7 @@ instance (Monad m, Explanation e) => MonadError e (ExplainT e m) where
     case y of
       (e, Nothing) -> do
         (e', z) <- runExplainT (f e)
-        pure (sequenced [e, e'], z)
+        pure (sequenced e e', z)
       z -> pure z
 
 -- * MonadExplain
@@ -148,93 +155,129 @@ instance MonadExplain e m => MonadExplain e (ReaderT r m) where
     withExplanation (runReaderT x r) (\stuff -> runReaderT (w stuff) r)
   mapExplanation f x = ReaderT $ \r -> mapExplanation f (runReaderT x r)
 
+enot :: (MonadExplain e m) => m a -> m ()
+enot x = withExplanation x $ \(e, res) -> case res of
+  Nothing -> success e ()
+  Just _ -> failure e
+
+guarded :: (Dualistic e, MonadExplain e m) => Bool -> e -> a -> m a
+guarded cond e tt  =
+  if cond
+  then success e tt
+  else failure (negated e)
+
 -- * An explanation type
+
+data Dual e = Pos e | Neg e
+  deriving stock (Eq, Ord, Show)
+
+instance Dualistic (Dual e) where
+  negated (Pos e) = Neg e
+  negated (Neg e) = Pos e
 
 data Expl cust
   = EPlain
   | EPar [Expl cust]
   | ESeq [Expl cust]
-  | EContrast (Expl cust) (Expl cust)
-  | ECustom cust
+  | ENeg (Expl cust)
+  | Custom cust
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable)
 
 customError :: (MonadError (Expl e) m) => e -> m a
-customError = throwError . ECustom
+customError = throwError . Custom
+
+instance Dualistic (Expl e) where
+  negated = ENeg
 
 instance Explanation (Expl cust) where
   plainly = EPlain
-  paralleled = EPar
-  sequenced = ESeq
-  contrasted = EContrast
+  paralleled x y = EPar [x, y]
+  sequenced x y = ESeq [x, y]
 
-{-
+instance Plated (Expl cust) where
+  plate :: Traversal' (Expl cust) (Expl cust)
+  plate f = \case
+    EPar es -> EPar <$> traverse f es
+    ESeq es -> ESeq <$> traverse f es
+    ENeg e -> ENeg <$> f e
+    e -> pure e
+
 instance Pretty cust => Pretty (Expl cust) where
   pretty = \case
     EPlain -> mempty
     EPar es -> bullets (pretty <$> es)
     ESeq es -> bulletItems " " "∴" (pretty <$> es)
-    EContrast e e' ->
-      vsep
-        [ "Have:",
-          indent 2 (pretty e),
-          "However:",
-          indent 2 (pretty e')
-        ]
-    ECustom cust -> pretty cust
--}
+    ENeg e -> "Not" <> colon <+> pretty e
+    Custom cust -> pretty cust
+
+bullets :: [Doc ann] -> Doc ann
+bullets = bulletItems "•" "•"
+
+bulletItems :: Text -> Text -> [Doc ann] -> Doc ann
+bulletItems _ _ [] = mempty
+bulletItems _ _ [x] = x
+bulletItems firstBullet bullet (f : xs) =
+  align (vsep (bulletItem firstBullet f : (bulletItem bullet <$> xs)))
+  where
+    bulletItem b x = pretty b <+> align x
 
 -- | The explanations produced are somewhat redundant, so they are optimised.
 --
--- >>> optimizeExpl ECustom (ESeq [EPar [ECustom "a", EPlain, EPar [ECustom "b"], ECustom "a"], ECustom "c"])
+-- >>> optimizeExpl (ESeq [EPar [ECustom "a", EPlain, EPar [ECustom "b"], ECustom "a"], ECustom "c"])
 -- ESeq [EPar [ECustom "a",ECustom "b"],ECustom "c"]
 --
 -- TODO(james): the following does a little /too much/ flattening for paralleled
 -- explanations. I think the solutions is to have to give the parallel
 -- combinator an extra flag argument to indicate if it wants to be flattened or
 -- not. Or maybe just have an extra 'Group' combinator.
-optimizeExpl :: forall a. (Ord a) => (a -> Expl a) -> Expl a -> Expl a
-optimizeExpl optCust = fixPoint (100 :: Int) (dedup . opt)
-  where
-    opt :: Expl a -> Expl a
-    opt e = unwrap $ case e of
-      EPlain -> EPlain
-      EPar es -> EPar $ seqopt (concatMap flatPar es)
-      ESeq es -> ESeq $ seqopt (concatMap flatSeq es)
-      EContrast e' e'' -> EContrast (opt e') (opt e'')
-      ECustom c -> optCust c
-    dedup :: Expl a -> Expl a
-    dedup e = evalState (dd e) mempty
-    dd :: Expl a -> State (Set (Expl a)) (Expl a)
-    dd expl = case expl of
-      EPar es -> EPar <$> ddseq es
-      ESeq es -> ESeq <$> ddseq es
-      EContrast e e' -> EContrast <$> dd e <*> dd e'
-      e -> pure e
-    ddseq :: [Expl a] -> State (Set (Expl a)) [Expl a]
-    ddseq [] = pure []
-    ddseq (e : es) = do
-      e' <- dd e
-      seen <- get
-      if Set.member e' seen
-        then ddseq es
-        else do
-          modify (Set.insert e')
-          (e' :) <$> ddseq es
-    seqopt es = filter keep (opt <$> es)
-    keep EPlain = False
-    keep (EPar []) = False
-    keep (ESeq []) = False
-    keep _ = True
-    unwrap (ESeq [e]) = e
-    unwrap (EPar [e]) = e
-    unwrap e = e
-    flatPar (EPar es) = concatMap flatPar es
-    flatPar e = [e]
-    flatSeq (ESeq es) = concatMap flatSeq es
-    flatSeq e = [e]
+optimizeExpl :: (Dualistic a, Ord a) => Expl a -> Expl a
+optimizeExpl = optimizeExplWith Custom
 
-    fixPoint 0 _ x = x
-    fixPoint bound f x =
-      if x == f x
-        then x
-        else fixPoint (bound - 1) f (f x)
+optimizeExplWith :: forall a. (Dualistic a, Ord a) => (a -> Expl a) -> Expl a -> Expl a
+optimizeExplWith f = fixPoint 100 (deduplicate . optimize)
+  where
+    -- [1] Full-tree, bottom-up transformations using functions in [2]
+    optimize, deduplicate :: Expl a -> Expl a
+    optimize = transform (custom f . squash . shrink)
+    deduplicate e = evalState (transformM dedup e) mempty
+    -- [2] Simple, non-recursive functions targeting a single node in the tree
+    dedup :: Expl a -> State (Set (Expl a)) (Expl a)
+    dedup e = do
+      seen <- get
+      if Set.member e seen
+        then pure EPlain
+        else modify (Set.insert e) >> pure e
+    custom :: (a -> Expl a) -> Expl a -> Expl a
+    custom optCust = \case
+      Custom c -> optCust c
+      e -> e
+    squash :: Expl a -> Expl a
+    squash = \case
+      EPar es -> EPar $ concatMap (\case EPar es' -> es'; e -> [e]) es
+      ESeq es -> ESeq $ concatMap (\case ESeq es' -> es'; e -> [e]) es
+      ENeg (ENeg e) -> e
+      ENeg (EPar es) -> EPar (ENeg <$> es)
+      ENeg (Custom e) -> Custom (negated e)
+      e -> e
+    shrink :: Expl a -> Expl a
+    shrink = \case
+      EPar [] -> EPlain
+      ESeq [] -> EPlain
+      EPar [e] -> e
+      ESeq [e] -> e
+      EPar es -> EPar $ filter hasInner es
+      ESeq es -> ESeq $ filter hasInner es
+      e -> e
+      where
+        hasInner = \case
+          EPlain -> False
+          EPar [] -> False
+          ESeq [] -> False
+          _ -> True
+
+fixPoint :: (Eq a) => Int -> (a -> a) -> (a -> a)
+fixPoint 0 _ x = x
+fixPoint bound f x =
+  if x == f x
+    then x
+    else fixPoint (bound - 1) f (f x)
---
+++ src/Example.hs
@@ -0,0 +1,197 @@
+{-# LANGUAGE ApplicativeDo #-}
+
+-- | An example of using ExplainT.
+module Example where
+
+import Control.Applicative
+import Control.Monad.Explain
+import Control.Monad.Identity
+import Control.Monad.Reader
+import Data.Bifunctor
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Text hiding (group)
+import Prettyprinter hiding (group)
+
+type Username = Text
+
+type PostID = Text
+
+type GroupID = Text
+
+data User = User
+  { username :: Username,
+    perms :: [Perm]
+  }
+
+data Group = Group
+  { groupID :: GroupID,
+    moderators :: [Username]
+  }
+
+data Perm = Admin | Edit
+  deriving stock (Eq, Ord, Show)
+
+instance Pretty Perm where
+  pretty =
+    squotes . \case
+      Admin -> "admin"
+      Edit -> "edit"
+
+data Post = Post
+  { postID :: PostID,
+    author :: Username,
+    group :: GroupID,
+    content :: Text
+  }
+
+data Fact
+  = IsAuthor Username PostID
+  | IsModerator Username GroupID
+  | HasPerm Username Perm
+  | NoPostWithID PostID
+  | NoUserWithUsername Username
+  | NoGroupWithID GroupID
+  | PostOfGroup PostID GroupID
+  deriving stock (Eq, Ord, Show)
+
+instance Pretty (Dual Fact) where
+  pretty = \case
+    Pos (IsAuthor u p) -> squotes (pretty u) <+> "authored post" <+> squotes (pretty p)
+    Neg (IsAuthor u p) -> squotes (pretty u) <+> "isn't the author of post" <+> squotes (pretty p)
+    Pos (IsModerator u g) -> squotes (pretty u) <+> "moderates" <+> squotes (pretty g)
+    Neg (IsModerator u g) -> squotes (pretty u) <+> "isn't a moderator of" <+> squotes (pretty g)
+    Pos (HasPerm u p) -> squotes (pretty u) <+> "has" <+> pretty p <+> "access"
+    Neg (HasPerm u p) -> squotes (pretty u) <+> "does not have" <+> pretty p <+> "access"
+    Pos (NoPostWithID i) -> "There is no post with ID" <> colon <+> squotes (pretty i)
+    Neg (NoPostWithID i) -> "Post" <+> squotes (pretty i) <+> "exists"
+    Pos (NoUserWithUsername u) -> "No user with username" <> colon <+> squotes (pretty u)
+    Neg (NoUserWithUsername u) -> "User" <+> squotes (pretty u) <+> "exists"
+    Pos (NoGroupWithID g) -> "No group with ID" <> colon <+> squotes (pretty g)
+    Neg (NoGroupWithID g) -> "group" <+> squotes (pretty g) <+> "exists"
+    Pos (PostOfGroup p g) -> "Post" <+> squotes (pretty p) <+> "belongs to group" <+> squotes (pretty g)
+    Neg (PostOfGroup p g) -> "Post" <+> squotes (pretty p) <+> "does not belong to group" <+> squotes (pretty g)
+
+data DB = DB
+  { users :: Map Username User,
+    posts :: Map PostID Post,
+    groups :: Map GroupID Group
+  }
+
+newtype App a = App {unApp :: ReaderT DB (ExplainT (Expl (Dual Fact)) Identity) a}
+  deriving newtype
+    ( Functor,
+      Applicative,
+      Alternative,
+      Monad,
+      MonadReader DB,
+      MonadExplain (Expl (Dual Fact))
+    )
+
+runApp :: App a -> (Expl (Dual Fact), Maybe a)
+runApp = first optimizeExpl . runIdentity . runExplainT . flip runReaderT db . unApp
+
+runAppIO :: (Show a) => App a -> IO ()
+runAppIO x = do
+  let (e, res) = runApp x
+  print (pretty e)
+  print res
+
+getThing :: (Ord thingID) => (DB -> Map thingID thing) -> (thingID -> Fact) -> thingID -> App thing
+getThing things eNoThing thingID = do
+  thing_ <- asks (Map.lookup thingID . things)
+  case thing_ of
+    Nothing -> failure (Custom (Pos (eNoThing thingID)))
+    Just thing -> pure thing
+
+getPost :: PostID -> App Post
+getPost = getThing posts NoPostWithID
+
+getGroup :: PostID -> App Group
+getGroup = getThing groups NoGroupWithID
+
+getUser :: Username -> App User
+getUser = getThing users NoUserWithUsername
+
+isAuthorOf :: User -> Post -> App ()
+isAuthorOf user post =
+  guarded
+    (author post == username user)
+    (Custom (Pos (IsAuthor (username user) (postID post))))
+    ()
+
+postGroup :: PostID -> App GroupID
+postGroup postID = do
+  post <- getPost postID
+  success (Custom (Pos (PostOfGroup postID (group post)))) (group post)
+
+isGroupModerator :: Username -> GroupID -> App ()
+isGroupModerator username groupID = do
+  group <- getGroup groupID
+  guarded
+    (username `elem` moderators group)
+    (Custom (Pos (IsModerator username groupID)))
+    ()
+
+moderatesGroupOfPost :: User -> PostID -> App ()
+moderatesGroupOfPost user postID = do
+  groupID <- postGroup postID
+  isGroupModerator (username user) groupID
+
+hasPerm :: User -> Perm -> App ()
+hasPerm user perm =
+  guarded
+    (perm `elem` perms user)
+    (Custom (Pos (HasPerm (username user) perm)))
+    ()
+
+-- A user may edit a post if:
+--  - They are the author; or,
+--  - They are a moderator of the group; or,
+--  - They are an admin.
+canEditPost :: Username -> PostID -> App ()
+canEditPost username postID = do
+  user <- getUser username
+  post <- getPost postID
+  (user `hasPerm` Edit)
+    >> ( user `isAuthorOf` post
+           <|> user `moderatesGroupOfPost` postID
+           <|> user `hasPerm` Admin
+       )
+
+canRequestEditAccess :: Username -> PostID -> App ()
+canRequestEditAccess username postID = enot (canEditPost username postID)
+
+db :: DB
+db =
+  DB
+    { users =
+        Map.fromList
+          [ ( "james",
+              User
+                { username = "james",
+                  perms = [Edit]
+                }
+            )
+          ],
+      posts =
+        Map.fromList
+          [ ( "explain",
+              Post
+                { postID = "explain",
+                  author = "julian",
+                  group = "haskell",
+                  content = "explain is awesome"
+                }
+            )
+          ],
+      groups =
+        Map.fromList
+          [ ( "haskell",
+              Group
+                { groupID = "haskell",
+                  moderators = ["simon"]
+                }
+            )
+          ]
+    }
